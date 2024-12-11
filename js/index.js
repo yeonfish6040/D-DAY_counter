dde2e62f11fd66893f777111f9f8e430156beb90
Yeonjun Lee <yeonfish6040@gmail.com>
2024-12-11 22:18:36 +0900

    feat: click to ticking

---
+++ js/index.js
@@ -0,0 +1,64 @@
+const dayFormat = /[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])/;
+const timeFormat = /([0-1][0-9]|2[0-3]):([0-5][0-9])/;
+let targetTimestamp = 0;
+let time = ``;
+
+let playsound = false;
+let ticking = true;
+let ticking2 = true;
+
+const ticking_common = new Audio("audio/ticking_common.wav");
+const ticking_close = new Audio("audio/ticking_close.wav");
+
+function init() {
+  if (localStorage.getItem("targetTimestamp"))
+    targetTimestamp = parseInt(localStorage.getItem("targetTimestamp"));
+  else {
+    let day = "";
+    while (!dayFormat.test(day)) {
+      day = prompt("날짜를 입력해주세요 (YYYY-MM-DD)");
+      if (!day)
+        return;
+    }
+    let time = "";
+    while (!timeFormat.test(time)) {
+      time = prompt("시간을 입력하세요 (HH:mm) 24h")
+      if (!day)
+        return;
+    }
+
+    targetTimestamp = moment(`${day} ${time}`, `YYYY-MM-DD HH:mm`).valueOf();
+    localStorage.setItem("targetTimestamp", targetTimestamp);
+  }
+  console.log(targetTimestamp);
+
+  ticking_common.loop = true;
+  ticking_close.loop = true;
+
+  $(".time").on("click", () => {
+    playsound = !playsound;
+    ticking = true;
+    ticking_common.currentTime = 0;
+    ticking_close.currentTime = 0;
+  })
+}
+
+function run() {
+  setInterval(() => {
+    const currentTime = moment().valueOf();
+    const diff = moment.duration(targetTimestamp - currentTime);
+    time = `${diff.hours().toString().padStart(2, "0")}:${diff.minutes().toString().padStart(2, "0")}:${diff.seconds().toString().padStart(2, "0")}`;
+    $(".time").text(time);
+    if (diff % 1000 < 150 && diff % 1000 > 50) {
+      if (playsound && ticking) {
+        ticking = false;
+        ticking_common.play();
+      }
+    }
+  }, 50);
+}
+
+// function mainTimeCountDown()
+
+init();
+run();
